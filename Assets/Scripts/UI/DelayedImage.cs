39b06f50a74d19722eb3b772dabd32291121e1a2
SeokbinKang <sean.kang84@gmail.com>
2019-06-24 02:58:55 -0400

    visual design improved

---
+++ Assets/Scripts/UI/DelayedImage.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DelayedImage : MonoBehaviour {
+
+    public float delay;
+    private float time_after_enable;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Time.time - time_after_enable >=delay)
+        {
+            RawImage ri = this.GetComponent<RawImage>();
+            if (ri == null) return;
+            Color c = Color.white;            
+            ri.color = c;
+            time_after_enable = float.MaxValue;
+        }
+		
+	}
+
+    private void OnEnable()
+    {
+        RawImage ri = this.GetComponent<RawImage>();
+        if (ri == null) return;
+        Color c = Color.white;
+        c.a = 0;
+        ri.color = c;
+        time_after_enable = Time.time;
+    }
+    private void OnDisable()
+    {
+        RawImage ri = this.GetComponent<RawImage>();
+        if (ri == null) return;
+        Color c = Color.white;
+        c.a = 0;
+        ri.color = c;
+    }
+
+}
